a173b914eeda70fc89007d4e8151864b6f6563fd
CyberEagle7 <himanshuupadhyay740@gmail.com>
2026-05-31 00:26:15 +0530

    Add files via upload

---
+++ ac.java
@@ -0,0 +1,18 @@
+import java.util.*;
+// import java.util.Scanner;
+
+public class ac{
+public static void avg(int a, int b, int c){
+    int avg = (a+b+c)/3;
+    System.out.println(avg);
+    return;
+}
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        avg(a, b, c);
+    }
+}
