c68df51c54bc10c8ff0c3af5d918f63aa01a6cda
Himanshu Upadhyay <himanshuupadhyay740@gmail.com>
2026-06-03 07:16:41 +0530

    Print root data after building the binary tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -30,5 +30,6 @@ public class BinaryTree {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = BinaryTrees.BuildTree(nodes);
+        System.out.println(root.data);
     }
 }
